f23caa668fdd958014d341d442299696eb7bdf03
RyQ <RyQ@NICKFURY>
2018-12-10 21:41:54 +0100

    Still in progress

--- wumpus/src/main/java/es/quiqueolaso/wumpus/utils/Option.java
+++ wumpus/src/main/java/es/quiqueolaso/wumpus/utils/Option.java
@@ -1,9 +1,20 @@
 package es.quiqueolaso.wumpus.utils;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Option.
+ */
 public class Option {
 
+	/** The opt. */
 	String flag, opt;
 
+	/**
+	 * Instantiates a new option.
+	 *
+	 * @param flag the flag
+	 * @param opt  the opt
+	 */
 	public Option(String flag, String opt) {
 		this.flag = flag;
 		this.opt = opt;
